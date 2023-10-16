20551623fd6624eb31985967c250cba0e7482d2a
Nima Ahmadi <147614675+Nima-Ahmadi-7602@users.noreply.github.com>
2023-10-16 18:19:59 +0330

    crating models

---
+++ src/app/models/IContact.ts
@@ -0,0 +1,10 @@
+export interface IContact {
+    id?: string;
+    name: string;
+    email: string;
+    photo: string;
+    company: string;
+    title: string;
+    address:string;
+    groupId: string;
+} 
