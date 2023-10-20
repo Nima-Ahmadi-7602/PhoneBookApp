7dba45b65c51f877436e25f7d7a4c74eb0cd73dd
Nima Ahmadi <147614675+Nima-Ahmadi-7602@users.noreply.github.com>
2023-10-20 18:31:11 +0330

    validations

--- src/app/components/add-contact/add-contact.component.ts
+++ src/app/components/add-contact/add-contact.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
-import {  Router } from '@angular/router';
+import { AbstractControl, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { IContact } from 'src/app/models/IContact';
 import { ContactService } from 'src/app/services/contact.service';
 
@@ -14,23 +14,75 @@ export class AddContactComponent implements OnInit {
   public contactId: string | null = null;
   public contact: IContact = {} as IContact;
   public errorMessage: string | null = null;
-  constructor(private contactService: ContactService, private router: Router , private formBuilder:FormBuilder) { }
 
-  ngOnInit(): void {
+  form: FormGroup = new FormGroup({
+    name: new FormControl(''),
+    email: new FormControl(''),
+    mobile: new FormControl(''),
+    title: new FormControl(''),
+    address: new FormControl(''),
+  });
+
+  submitted = false;
+  constructor(private contactService: ContactService, private router: Router, private formBuilder: FormBuilder) {
+
   }
 
-  public createSbmit() {
-    this.contactService.createContact(this.contact).subscribe((data) => {
+  ngOnInit(): void {
+    this.form = this.formBuilder.group(
+      {
+        name: ['', Validators.required, Validators.maxLength(30)],
+        mobile: [
+          '',
+          [
+            Validators.required,
+            Validators.maxLength(11)          
+          ]
+        ],
+        email: ['', [Validators.required, Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,4}$")]],
+        title: [
+          '',
+          [
+            Validators.required,
+            Validators.minLength(3),
+            Validators.maxLength(30)
+          ]
+        ],
+        address: [
+          '',
+          [
+            Validators.required,
+            Validators.minLength(6),
+            Validators.maxLength(50)
+          ]
+        ],
+      }
+    );
+  }
+
+  get f(): { [key: string]: AbstractControl } {
+    return this.form.controls;
+  }
+
+  onSubmit(): void {
+    this.submitted = true;
+
+    if (this.form.invalid) {
+      return;
+    }else{    this.contactService.createContact(this.contact).subscribe((data) => {
       this.router.navigate(['/']).then();
     }, (error) => {
       this.errorMessage = error;
       this.router.navigate(['/contact/add']).then();
-
     });
+
+    }
+
+    console.log(JSON.stringify(this.form.value, null, 2));
   }
-  addForm!:FormGroup;
-  submited=false;
 
-
+  onReset(): void {
+    this.submitted = false;
+    this.form.reset();
+  }
 }
-
